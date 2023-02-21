c5103120d337f275c72e1e7ac817eaf5a4d447ac
Weriks Santos <werikscs@gmail.com>
2023-02-21 09:47:40 -0300

    feat: show previous messages when user connect

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,18 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+const messagesArray: {
+  messageAuhor: string
+  messageDate: Date
+  message: any
+}[] = []
+
 app.use(express.static('src/public'))
 
 io.on('connection', (socket) => {
   socket.broadcast.emit('user-connected', socket.id)
+
+  socket.emit('load-prev-messages', messagesArray)
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', socket.id)
@@ -18,7 +26,9 @@ io.on('connection', (socket) => {
   socket.on('chat message', (message) => {
     const messageAuhor = socket.id
     const messageDate = new Date()
-    io.emit('chat message', { messageAuhor, messageDate, message })
+    const messageObj = { messageAuhor, messageDate, message }
+    messagesArray.push(messageObj)
+    io.emit('chat message', { ...messageObj })
   })
 
   socket.on('user typing', () => {
